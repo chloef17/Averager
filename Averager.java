8036767eec562c5e94371c996f091dcaec24d793
chloef17 <chloe_fink@asl.org>
2014-10-14 09:06:12 +0100

    Completed Averager
    I wrote the code to complete a averager machine

--- Averager.java
+++ Averager.java
@@ -2,15 +2,25 @@
 *
 * Reads in scores until a negative value is encountered, at which point it determines and prints the average
 *
-* @author <your name here>
-* @version <date you finished the code>
+* @author <Chloe Fink>
+* @version <14th October 2014>
 *
 */
+import java.util.Scanner;
 
-public class Averager {
-	
-	public static void main(String[] args) {
-		// TODO - Put your code here
-	}
-
-}
+public class Averager{
+   public static void main (String [] args){
+   Scanner scnr = new Scanner(System.in);
+   double averageScores = 0;
+   
+   System.out.print("Please enter your test scores, or -5 to quit.");
+   double addScores = scnr.nextInt();
+   while (addScores != -5) {
+      averageScores = averageScores + addScores/3;
+      System.out.println("Please enter another score or -5 to quit.");
+      addScores = scnr.nextDouble();
+   }
+   
+   System.out.println("The averagae of your scores is " +averageScores);
+   }
+ }
